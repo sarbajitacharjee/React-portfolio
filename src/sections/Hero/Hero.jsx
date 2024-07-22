e4df4853e16999074a4321a98c118960ff9c3a1e
Sarbajit Acharjee <sarbajitacharjee712@gmail.com>
2024-07-22 15:17:54 +0530

    new updates

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,7 +8,7 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/cv.pdf';
+import Resume from '../../assets/Resume.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
@@ -25,7 +25,7 @@ function Hero() {
         <img
           src={heroImg}
           className={styles.hero}
-          alt="Profile picture of Harris Johnsen"
+          alt="Profile picture of Sarbajit Acharjee"
         />
         <img
           className={styles.colorMode}
@@ -36,27 +36,26 @@ function Hero() {
       </div>
       <div className={styles.info}>
         <h1>
-          Harris
+          Sarbajit
           <br />
-          Johnsen
+          Acharjee
         </h1>
         <h2>Frontend Developer</h2>
         <span>
-          <a href="https://twitter.com/" target="_blank">
+          <a href="https://x.com/SarbajitAcharj2" target="_blank">
             <img src={twitterIcon} alt="Twitter icon" />
           </a>
-          <a href="https://github.com/" target="_blank">
+          <a href="https://github.com/sarbajitacharjee" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://linkedin.com/" target="_blank">
+          <a href="https://www.linkedin.com/in/sarbajit-achariya25/" target="_blank">
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
         <p className={styles.description}>
-          With a passion for developing modern React web apps for commercial
-          businesses.
+          With a passion for developing modern website ...
         </p>
-        <a href={CV} download>
+        <a href={Resume} download>
           <button className="hover">Resume</button>
         </a>
       </div>
