d6ac575750facfbca40a21c0f4e1fc3ac755df22
Sarbajit Acharjee <sarbajitacharjee712@gmail.com>
2024-08-02 00:40:45 +0530

    add

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -8,7 +8,8 @@ import ProjectCard from '../../common/ProjectCard';
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
-      <h1 className="sectionTitle">Projects</h1>
+      <h1 className="sectionTitle">Projects  </h1>
+      <span style={{color:"red",fontSize:"24px"}}>Click Icon To View</span>
       <div className={styles.projectsContainer}>
         <ProjectCard
           src={logo}
